4948755310324eae2ffdf8f7fd487973bb32223d
Carlos <carloslml90@gmail.com>
2023-04-15 11:04:32 -0400

    stricmode

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -9,9 +9,9 @@ import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-
+<React.StrictMode>
 <CounterApp value={10} />
+</React.StrictMode>
 
 )
-
 
